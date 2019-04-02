3133d494a45f369e3a9e8c61cc85635b6949489a
Guillem <guillem@disedit.com>
2019-04-02 09:14:24 +0200

    Add react-reveal

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,16 +1,17 @@
 import React from "react"
 import IntLink from "./IntLink"
 import Imparables from "./elements/imparables"
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage } from "react-intl"
+import Fade from "react-reveal/Fade"
 
 const Hero = () => (
   <div className="hero">
     <div className="container-fluid">
-      <div className="hero-claim"><Imparables /></div>
-      <h2><FormattedMessage id="hero_intro" /></h2>
-      <ul class="hero-ctas">
-        <li><IntLink to="/hello" className="hero-cta hero-cta--primary"><FormattedMessage id="hero_cta1" /></IntLink></li>
-        <li><IntLink to="/hello" className="hero-cta"><FormattedMessage id="hero_cta2" /></IntLink></li>
+      <div className="hero-claim"><Fade duration={2000}><Imparables /></Fade></div>
+      <h2><Fade bottom delay={500}><FormattedMessage id="hero_intro" /></Fade></h2>
+      <ul className="hero-ctas">
+        <li><Fade bottom delay={700}><IntLink to="/hello" className="hero-cta hero-cta--primary"><FormattedMessage id="hero_cta1" /></IntLink></Fade></li>
+        <li><Fade bottom delay={900}><IntLink to="/hello" className="hero-cta"><FormattedMessage id="hero_cta2" /></IntLink></Fade></li>
       </ul>
     </div>
   </div>
