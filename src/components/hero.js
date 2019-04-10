3bb412361c5833b2dcb4d3a817dc8597dfceb36e
Guillem <guillem@disedit.com>
2019-04-10 19:45:26 +0200

    Add webm version to Hero video

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,15 +4,25 @@ import Imparables from "./elements/imparables"
 import { FormattedMessage } from "react-intl"
 import Fade from "react-reveal/Fade"
 
+import VideoMp4 from "../video/hero.mp4"
+import VideoWebM from "../video/hero.webm"
+import VideoCover from "../images/candidates/monica-oltra.jpg"
+
 const Hero = () => (
   <div className="hero">
-    <div className="container-fluid">
+    <div className="container-fluid hero-content">
       <div className="hero-claim"><Fade duration={2000}><Imparables /></Fade></div>
       <h2><Fade bottom delay={500}><FormattedMessage id="hero_intro" /></Fade></h2>
       <ul className="hero-ctas">
         <li><Fade bottom delay={700}><IntLink to="/candidatura" className="hero-cta hero-cta--primary"><FormattedMessage id="hero_cta1" /></IntLink></Fade></li>
         <li><Fade bottom delay={900}><IntLink to="/programa" className="hero-cta"><FormattedMessage id="hero_cta2" /></IntLink></Fade></li>
       </ul>
+    </div>
+    <div className="hero-video">
+      <video autoPlay loop poster={VideoCover}>
+        <source src={VideoWebM} type="video/webm" />
+        <source src={VideoMp4} type="video/mp4" />
+      </video>
     </div>
   </div>
 )
