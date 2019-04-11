7f0a11987bc622241a9b37ecafbf2df5b7df724b
Guillem <guillem@disedit.com>
2019-04-11 22:40:24 +0200

    Update seo metatags

--- src/components/seo.js
+++ src/components/seo.js
@@ -44,7 +44,7 @@ function SEO({ description, lang, meta, keywords, title, image }) {
         },
         {
           property: `og:title`,
-          content: title,
+          content: title || site.siteMetadata.title,
         },
         {
           property: `og:description`,
